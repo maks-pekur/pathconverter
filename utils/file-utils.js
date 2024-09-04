59482dc05904324faa6c58fd0a182879a7e9910e
maks_p <maksym@9d.pro>
2024-09-04 15:49:28 +0200

    fix

--- utils/file-utils.js
+++ utils/file-utils.js
@@ -1,29 +1,47 @@
-import fs from 'fs-extra'
-import path from 'path'
+import fs from "fs-extra";
+import path from "path";
 
-function safeStatSync(path) {
-	try {
-		return fs.statSync(path)
-	} catch (error) {
-		if (error.code === 'EACCES') {
-			logger.log(`❌ Недостаточно прав для доступа к файлу: ${path}`)
-		} else {
-			throw error
-		}
-	}
+import logger from "./logger.js";
+
+/**
+ * Функция для безопасного получения статистики файла с установкой прав доступа.
+ * Если возникла ошибка доступа, она сообщает об этом в лог.
+ * @param {string} filePath - Путь к файлу
+ * @returns {fs.Stats | null} - Статистика файла, если успешно
+ */
+
+function safeStatSync(filePath) {
+  try {
+    fs.chmodSync(filePath, 0o777);
+    return fs.statSync(filePath);
+  } catch (error) {
+    if (error.code === "EACCES") {
+      logger.log(
+        `❌ Недостаточно прав для доступа к файлу: ${path.basename(filePath)}`
+      );
+    } else {
+      throw error;
+    }
+    return null;
+  }
 }
 
+/**
+ * Функция для рекурсивного обхода директорий.
+ * Возвращает массив путей всех файлов в директории.
+ * @param {string} dir - Путь к директории
+ * @returns {string[]} - Массив путей к файлам
+ */
 export function walkSync(dir) {
-	let files = []
-	fs.readdirSync(dir).forEach(file => {
-		const fullPath = path.join(dir, file)
-		const stat = safeStatSync(fullPath)
-		if (stat && stat.isDirectory()) {
-			files = files.concat(walkSync(fullPath))
-		} else if (stat) {
-			files.push(fullPath)
-		}
-	})
-	return files
+  let files = [];
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+    const stat = safeStatSync(fullPath);
+    if (stat && stat.isDirectory()) {
+      files = files.concat(walkSync(fullPath));
+    } else if (stat) {
+      files.push(fullPath);
+    }
+  });
+  return files;
 }
-
--- utils/replace-to-absolute-path.js
+++ utils/replace-to-absolute-path.js
@@ -1,206 +1,219 @@
-import fs from 'fs'
-import path from 'path'
-import { walkSync } from './file-utils.js'
-import logger from './logger.js'
+import fs from "fs";
+import path from "path";
+import { walkSync } from "./file-utils.js";
+import logger from "./logger.js";
 
 function shouldExcludePath(filePath) {
-	return (
-		filePath.startsWith('http') ||
-		filePath.startsWith('//') ||
-		filePath.startsWith('/') ||
-		filePath.includes('fonts.googleapis.com') ||
-		filePath.includes('cdnjs.cloudflare.com') ||
-		filePath.startsWith('data:image') ||
-		filePath.startsWith('tel:') ||
-		filePath.startsWith('mailto:') ||
-		filePath.startsWith('javascript:void(0)') ||
-		filePath.startsWith('#form') ||
-		filePath === '#' ||
-		filePath.trim() === ''
-	)
+  return (
+    filePath.startsWith("http") ||
+    filePath.startsWith("//") ||
+    filePath.startsWith("/") ||
+    filePath.includes("fonts.googleapis.com") ||
+    filePath.includes("cdnjs.cloudflare.com") ||
+    filePath.startsWith("data:image") ||
+    filePath.startsWith("tel:") ||
+    filePath.startsWith("mailto:") ||
+    filePath.startsWith("javascript:void(0)") ||
+    filePath.startsWith("#form") ||
+    filePath === "#" ||
+    filePath.trim() === ""
+  );
 }
 
 function processAttributes(content, fileDir, basePath, project, taskType) {
-	return content.replace(
-		/(href|src|action|data-background)=['"](.*?)['"]/g,
-		(match, attr, relativePath, offset, string) => {
-			const isCanonical = string.slice(0, offset).includes('rel="canonical"')
-			if (isCanonical || shouldExcludePath(relativePath)) return match
+  return content.replace(
+    /(href|src|action|data-background)=['"](.*?)['"]/g,
+    (match, attr, relativePath, offset, string) => {
+      const isCanonical = string.slice(0, offset).includes('rel="canonical"');
+      if (isCanonical || shouldExcludePath(relativePath)) return match;
 
-			// Если это якорь `href="#something"`, добавляем перед ним `index.html`
-			if (relativePath.startsWith('#')) {
-				const relativeToRoot = path.relative(project, fileDir)
-				return `${attr}="${basePath}/${relativeToRoot}/index.html${relativePath}"`
-			}
+      // Если это якорь `href="#something"`, добавляем перед ним `index.html`
+      if (relativePath.startsWith("#")) {
+        const relativeToRoot = path.relative(project, fileDir);
+        return `${attr}="${basePath}/${relativeToRoot}/index.html${relativePath}"`;
+      }
 
-			// Обрабатываем пути, начинающиеся с '../' и с любым количеством вложенности
-			if (relativePath.startsWith('../')) {
-				let absolutePath = path.resolve(fileDir, relativePath)
-				// Если в относительном пути есть якорь, добавляем index.html перед якорем
-				if (relativePath.includes('#')) {
-					const [pathPart, anchor] = relativePath.split('#')
-					absolutePath =
-						path.resolve(fileDir, pathPart) + '/index.html#' + anchor
-				} else {
-					absolutePath = path.resolve(fileDir, relativePath)
-				}
-				const relativeToRoot = path.relative(project, absolutePath)
-				return `${attr}="${basePath}/${relativeToRoot}"`
-			}
+      // Обрабатываем пути, начинающиеся с '../' и с любым количеством вложенности
+      if (relativePath.startsWith("../")) {
+        let absolutePath = path.resolve(fileDir, relativePath);
+        // Если в относительном пути есть якорь, добавляем index.html перед якорем
+        if (relativePath.includes("#")) {
+          const [pathPart, anchor] = relativePath.split("#");
+          absolutePath =
+            path.resolve(fileDir, pathPart) + "/index.html#" + anchor;
+        } else {
+          absolutePath = path.resolve(fileDir, relativePath);
+        }
+        const relativeToRoot = path.relative(project, absolutePath);
+        return `${attr}="${basePath}/${relativeToRoot}"`;
+      }
 
-			// Обрабатываем обычные пути, заменяя .php на .html
-			if (taskType === 'websites') {
-				relativePath = relativePath.replace(/\.php$/, '.html')
-			}
+      // Обрабатываем обычные пути, заменяя .php на .html
+      if (taskType === "websites") {
+        relativePath = relativePath.replace(/\.php$/, ".html");
+      }
 
-			let absolutePath
+      let absolutePath;
 
-			if (relativePath === './' || relativePath === '.') {
-				absolutePath = path.join(fileDir, 'index.html')
-			} else if (relativePath.startsWith('#')) {
-				absolutePath = path.join(fileDir, 'index.html') + relativePath
-			} else {
-				absolutePath = path.resolve(fileDir, relativePath)
-			}
+      if (relativePath === "./" || relativePath === ".") {
+        absolutePath = path.join(fileDir, "index.html");
+      } else if (relativePath.startsWith("#")) {
+        absolutePath = path.join(fileDir, "index.html") + relativePath;
+      } else {
+        absolutePath = path.resolve(fileDir, relativePath);
+      }
 
-			const relativeToRoot = path.relative(project, absolutePath)
-			return `${attr}="${basePath}/${relativeToRoot}"`
-		}
-	)
+      const relativeToRoot = path.relative(project, absolutePath);
+      return `${attr}="${basePath}/${relativeToRoot}"`;
+    }
+  );
 }
 
 function processSrcset(content, fileDir, basePath, project, taskType) {
-	return content.replace(/srcset=['"](.*?)['"]/g, (match, srcset) => {
-		const updatedSrcset = srcset
-			.split(',')
-			.map(entry => {
-				let [url, descriptor] = entry.trim().split(/\s+/)
+  return content.replace(/srcset=['"](.*?)['"]/g, (match, srcset) => {
+    const updatedSrcset = srcset
+      .split(",")
+      .map((entry) => {
+        let [url, descriptor] = entry.trim().split(/\s+/);
 
-				if (shouldExcludePath(url)) return entry.trim()
+        if (shouldExcludePath(url)) return entry.trim();
 
-				if (taskType === 'websites') {
-					url = url.replace(/\.php$/, '.html')
-				}
+        if (taskType === "websites") {
+          url = url.replace(/\.php$/, ".html");
+        }
 
-				let absolutePath
-				if (url === './' || url === '.') {
-					absolutePath = path.join(fileDir, 'index.html')
-				} else if (url.startsWith('#')) {
-					absolutePath = path.join(fileDir, 'index.html') + url
-				} else {
-					absolutePath = path.resolve(fileDir, url)
-				}
+        let absolutePath;
+        if (url === "./" || url === ".") {
+          absolutePath = path.join(fileDir, "index.html");
+        } else if (url.startsWith("#")) {
+          absolutePath = path.join(fileDir, "index.html") + url;
+        } else {
+          absolutePath = path.resolve(fileDir, url);
+        }
 
-				const relativeToRoot = path.relative(project, absolutePath)
-				return `${basePath}/${relativeToRoot} ${descriptor || ''}`.trim()
-			})
-			.join(', ')
+        const relativeToRoot = path.relative(project, absolutePath);
+        return `${basePath}/${relativeToRoot} ${descriptor || ""}`.trim();
+      })
+      .join(", ");
 
-		return `srcset="${updatedSrcset}"`
-	})
+    return `srcset="${updatedSrcset}"`;
+  });
 }
 
 function processCssUrls(content, fileDir, basePath, project, taskType) {
-	return content.replace(
-		/url\((['"]?)(.*?)\1\)/g,
-		(match, quote, relativePath) => {
-			if (shouldExcludePath(relativePath)) return match
+  return content.replace(
+    /url\((['"]?)(.*?)\1\)/g,
+    (match, quote, relativePath) => {
+      if (shouldExcludePath(relativePath)) return match;
 
-			if (taskType === 'websites') {
-				relativePath = relativePath.replace(/\.php$/, '.html')
-			}
+      if (taskType === "websites") {
+        relativePath = relativePath.replace(/\.php$/, ".html");
+      }
 
-			let absolutePath
-			if (relativePath === './' || relativePath === '.') {
-				absolutePath = path.join(fileDir, 'index.html')
-			} else {
-				absolutePath = path.resolve(fileDir, relativePath)
-			}
+      let absolutePath;
+      if (relativePath === "./" || relativePath === ".") {
+        absolutePath = path.join(fileDir, "index.html");
+      } else {
+        absolutePath = path.resolve(fileDir, relativePath);
+      }
 
-			const relativeToRoot = path.relative(project, absolutePath)
-			return `url(${basePath}/${relativeToRoot})`
-		}
-	)
+      const relativeToRoot = path.relative(project, absolutePath);
+      return `url(${basePath}/${relativeToRoot})`;
+    }
+  );
 }
 
 function processInlineStyles(content, fileDir, basePath, project, taskType) {
-	return content.replace(/style=['"](.*?)['"]/g, (match, styleContent) => {
-		const updatedStyleContent = styleContent.replace(
-			/url\((['"]?)(.*?)\1\)/g,
-			(styleMatch, quote, relativePath) => {
-				if (shouldExcludePath(relativePath)) return styleMatch
+  return content.replace(/style=['"](.*?)['"]/g, (match, styleContent) => {
+    const updatedStyleContent = styleContent.replace(
+      /url\((['"]?)(.*?)\1\)/g,
+      (styleMatch, quote, relativePath) => {
+        if (shouldExcludePath(relativePath)) return styleMatch;
 
-				if (taskType === 'websites') {
-					relativePath = relativePath.replace(/\.php$/, '.html')
-				}
+        if (taskType === "websites") {
+          relativePath = relativePath.replace(/\.php$/, ".html");
+        }
 
-				let absolutePath
-				if (relativePath === './' || relativePath === '.') {
-					absolutePath = path.join(fileDir, 'index.html')
-				} else {
-					absolutePath = path.resolve(fileDir, relativePath)
-				}
+        let absolutePath;
+        if (relativePath === "./" || relativePath === ".") {
+          absolutePath = path.join(fileDir, "index.html");
+        } else {
+          absolutePath = path.resolve(fileDir, relativePath);
+        }
 
-				const relativeToRoot = path.relative(project, absolutePath)
-				return `url(${basePath}/${relativeToRoot})`
-			}
-		)
-		return `style="${updatedStyleContent}"`
-	})
+        const relativeToRoot = path.relative(project, absolutePath);
+        return `url(${basePath}/${relativeToRoot})`;
+      }
+    );
+    return `style="${updatedStyleContent}"`;
+  });
 }
 
 function processJsPaths(content, fileDir, basePath, project, taskType) {
-	return content.replace(
-		/(window\.location\.href|location)=['"](.*?)['"]/g,
-		(match, attr, relativePath) => {
-			if (shouldExcludePath(relativePath)) return match
+  return content.replace(
+    /(window\.location\.href|location)=['"](.*?)['"]/g,
+    (match, attr, relativePath) => {
+      if (shouldExcludePath(relativePath)) return match;
 
-			if (taskType === 'websites') {
-				relativePath = relativePath.replace(/\.php$/, '.html')
-			}
+      if (taskType === "websites") {
+        relativePath = relativePath.replace(/\.php$/, ".html");
+      }
 
-			let absolutePath
-			if (relativePath === './' || relativePath === '.') {
-				absolutePath = path.join(fileDir, 'index.html')
-			} else {
-				absolutePath = path.resolve(fileDir, relativePath)
-			}
+      let absolutePath;
+      if (relativePath === "./" || relativePath === ".") {
+        absolutePath = path.join(fileDir, "index.html");
+      } else {
+        absolutePath = path.resolve(fileDir, relativePath);
+      }
 
-			const relativeToRoot = path.relative(project, absolutePath)
-			return `${attr}="${basePath}/${relativeToRoot}"`
-		}
-	)
+      const relativeToRoot = path.relative(project, absolutePath);
+      return `${attr}="${basePath}/${relativeToRoot}"`;
+    }
+  );
 }
 
+/**
+ * Функция замены относительных путей на абсолютные с учётом taskType.
+ * @param {string} project - Путь к проекту
+ * @param {string} basePath - Базовый путь для замены
+ * @param {string} taskType - Тип задачи (например, websites)
+ */
+
 export function replaceToAbsolutePath(project, basePath, taskType) {
-	logger.log(`\n👉 Меняю пути на абсолютные...`)
+  logger.log(`\n👉 Меняю пути на абсолютные...`);
 
-	const files = walkSync(project)
+  const files = walkSync(project);
 
-	files.forEach(file => {
-		if (
-			file.endsWith('.html') ||
-			file.endsWith('.php') ||
-			file.endsWith('.css') ||
-			file.endsWith('.js')
-		) {
-			let content = fs.readFileSync(file, 'utf-8')
-			const fileDir = path.dirname(file)
+  files.forEach((file) => {
+    if (
+      file.endsWith(".html") ||
+      file.endsWith(".php") ||
+      file.endsWith(".css") ||
+      file.endsWith(".js")
+    ) {
+      let content = fs.readFileSync(file, "utf-8");
+      const fileDir = path.dirname(file);
 
-			content = processAttributes(content, fileDir, basePath, project, taskType)
-			content = processSrcset(content, fileDir, basePath, project, taskType)
-			content = processCssUrls(content, fileDir, basePath, project, taskType)
-			content = processInlineStyles(
-				content,
-				fileDir,
-				basePath,
-				project,
-				taskType
-			)
-			content = processJsPaths(content, fileDir, basePath, project, taskType)
+      content = processAttributes(
+        content,
+        fileDir,
+        basePath,
+        project,
+        taskType
+      );
+      content = processSrcset(content, fileDir, basePath, project, taskType);
+      content = processCssUrls(content, fileDir, basePath, project, taskType);
+      content = processInlineStyles(
+        content,
+        fileDir,
+        basePath,
+        project,
+        taskType
+      );
+      content = processJsPaths(content, fileDir, basePath, project, taskType);
 
-			fs.writeFileSync(file, content, 'utf-8')
-			logger.log(`✅ ${path.basename(file)} успешно обработан.`)
-		}
-	})
+      fs.writeFileSync(file, content, "utf-8");
+      logger.log(`✅ ${path.basename(file)} успешно обработан.`);
+    }
+  });
 }
